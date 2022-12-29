74df2889a8927c4250f070363a7034be2a0cce7c
abdo-developer0 <tknwtknwlwjy10@gmail.com>
2022-12-29 22:20:11 +0200

    fix200 response code

--- index.php
+++ index.php
@@ -5,21 +5,22 @@
  * for get usful functions
  */
 
- require_once __DIR__ . '/helpers.php';
+require_once __DIR__ . '/helpers.php';
 
 /**
  * start output buffering for errors
  */
 
- ob_start();
+ob_start();
 
 if (route('/') or route('/home')) {
 
-   $visitors = require_once( __DIR__ . '/__on_visit.php');
+  $visitors = require_once(__DIR__ . '/__on_visit.php');
 
-   html_render('home.html', ['visitors' => $visitors]);
+  html_render('home.html', ['visitors' => $visitors]);
+  http_response_code(200);
+  
+} else {
+  html_render('404.html');
+  http_response_code(404);
 }
- else {
-   html_render('404.html');
-   http_response_code(404);
- }
