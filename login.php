cf584bc3745e61b5d99470f1ee8da925e32d5332
abdo-developer0 <tknwtknwlwjy10@gmail.com>
2022-12-31 04:47:25 +0200

    update project.

---
+++ login.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>login</title>
+
+    <link rel="stylesheet" href="bootstrap.css">
+</head>
+<body>
+    <div class="vh-100 d-flex justify-content-center align-items-center">
+        <form action="" style="max-width: 400px;">
+            <h1 class="display-2 text-center pb-5">Log in</h1>
+            <input type="email" class="form-control mb-3 rounded" placeholder="Email" name="email">
+            <input type="text" class="form-control mb-3 rounded" placeholder="Password" name="password">
+            
+            <button class="form-control btn btn-danger py-2 rounded" type="submit">Log in</button>
+        </form>
+    </div>
+</body>
+</html>
